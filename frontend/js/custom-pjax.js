61d1e7b60827b562fd94616b9b66e5b2a9c19c3a
Killera07 <81530167+Killera07@users.noreply.github.com>
2021-09-05 20:38:18 +0300

    Add files via upload

---
+++ frontend/js/custom-pjax.js
@@ -0,0 +1,1 @@
+(function($){"use strict";$.pjax.defaults.timeout=90000;$(document).pjax('a.pjax','#pjax-container');$(document).on('pjax:start',function(){NProgress.start();$('.video-empty').html('');});$(document).on('pjax:end',function(){NProgress.done();$('.video-empty').html('');});})(jQuery);
