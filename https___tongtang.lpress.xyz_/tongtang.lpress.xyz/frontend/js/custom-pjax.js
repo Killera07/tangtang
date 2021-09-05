84229f5dfcde76f42fb8eef72593478b0dac9e62
Killera07 <81530167+Killera07@users.noreply.github.com>
2021-09-05 20:33:45 +0300

    Delete https___tongtang.lpress.xyz_ directory

--- https___tongtang.lpress.xyz_/tongtang.lpress.xyz/frontend/js/custom-pjax.js
+++
@@ -1,1 +0,0 @@
-(function($){"use strict";$.pjax.defaults.timeout=90000;$(document).pjax('a.pjax','#pjax-container');$(document).on('pjax:start',function(){NProgress.start();$('.video-empty').html('');});$(document).on('pjax:end',function(){NProgress.done();$('.video-empty').html('');});})(jQuery);
