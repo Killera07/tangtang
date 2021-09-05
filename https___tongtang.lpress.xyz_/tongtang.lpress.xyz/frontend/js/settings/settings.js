0dace406a6b99772b65f63fcbc31b6ff985fc674
Killera07 <81530167+Killera07@users.noreply.github.com>
2021-09-05 20:32:00 +0300

    Add files via upload

---
+++ https___tongtang.lpress.xyz_/tongtang.lpress.xyz/frontend/js/settings/settings.js
@@ -0,0 +1,6 @@
+(function($){"use strict";function settings(url,formdata)
+{$.ajaxSetup({headers:{'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')}});$.ajax({url:url,data:formdata,type:"POST",dataType:"JSON",processData:false,contentType:false,beforeSend:function(){$('.usersettings_update').html('Please Wait...');},success:function(response){if(response.errors)
+{$('.error-message-area').fadeIn();$('.error-msg').html(response.errors);$(".error-message-area").delay(2000).fadeOut(2000);$('.usersettings_update').html('Update');}
+if(response=='ok')
+{$('#current_password').val('');$('#password').val('');$('#password_confirmation').val('');$('.alert-message-area').fadeIn();$(".alert-message-area").delay(2000).fadeOut(2000);$('.usersettings_update').html('Update');}}});}
+$(document).on('pjax:complete',function(){$('form.setting_form').on('submit',function(e){e.preventDefault();var url=this.action;var formdata=new FormData(this);settings(url,formdata);});});$('form.setting_form').on('submit',function(e){e.preventDefault();var url=this.action;var formdata=new FormData(this);settings(url,formdata);});})(jQuery);
